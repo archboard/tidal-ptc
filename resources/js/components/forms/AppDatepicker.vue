ff012b64c8defa42d0422beb5328a29ef69f5dff
Grant Holle <hollegrant@gmail.com>
2023-12-19 14:13:17 +0400

    Improve date picker

--- resources/js/components/forms/AppDatepicker.vue
+++ resources/js/components/forms/AppDatepicker.vue
@@ -1,20 +1,23 @@
 <template>
   <DatePicker
-    v-model="localValue"
-    :popover="{ visibility: 'focus' }"
-    :is-dark="theme === 'dark'"
+    v-model.string="localValue"
+    :masks="masks"
     v-bind="$attrs"
+    :is-dark="isDark"
+    color="primary"
+    :popover="popoverOptions"
+    :mode="mode"
   >
     <template v-slot="{ inputValue, inputEvents }">
       <div class="relative">
-        <div class="absolute left-0 inset-y-0 flex items-center pl-3 pointer-events-none">
-          <CalendarIcon class="text-gray-500 h-4 w-4" />
+        <div :class="['absolute left-0 inset-y-0 flex items-center pl-3 pointer-events-none', hasError ? 'text-red-500' : 'text-gray-500']">
+          <CalendarDaysIcon class="h-4 w-4" />
         </div>
 
-        <AppInput class="pl-10" :value="inputValue" v-on="inputEvents" :has-error="hasError" />
+        <AppInput class="pl-10" :value="inputValue" v-on="inputEvents" :id="id" :has-error="hasError" :disabled="disabled" />
 
-        <div class="absolute right-0 inset-y-0 flex items-center justify-center" :class="{ 'pr-4': !hasError, 'pr-8': hasError }">
-          <button @click.prevent="localValue = null" class="flex items-center justify-center">
+        <div class="absolute right-0 inset-y-0 flex items-center justify-center" :class="{ 'pr-4': !hasError, 'pr-10': hasError }">
+          <button @click.prevent="clearValue" class="flex items-center justify-center">
             <span class="sr-only">Clear date</span>
             <TrashIcon class="text-red-500 h-4 w-4" />
           </button>
@@ -30,15 +33,35 @@ import 'v-calendar/dist/style.css'
 import AppInput from '@/components/forms/AppInput.vue'
 import { fieldProps, fieldEmits } from '@/composition/useFormField.js'
 import { useVModel } from '@vueuse/core'
-import { TrashIcon, CalendarIcon } from '@heroicons/vue/24/outline'
-import { useLocalStorage } from '@vueuse/core'
+import { CalendarDaysIcon } from '@heroicons/vue/24/outline'
+import { TrashIcon } from '@heroicons/vue/24/solid'
+import useColorTheme from '@/composition/useColorTheme.js'
+import { watch } from 'vue'
 
-const theme = useLocalStorage('theme', 'light')
 const props = defineProps({
   ...fieldProps,
+  mode: {
+    type: String,
+    default: 'date',
+  }
 })
 const emit = defineEmits([
   ...fieldEmits,
 ])
 const localValue = useVModel(props, 'modelValue', emit)
+const { isDark } = useColorTheme()
+const masks = {
+  modelValue: props.mode.toLowerCase().includes('time') ? 'YYYY-MM-DD HH:mm' : 'YYYY-MM-DD',
+}
+const popoverOptions = {
+  visibility: 'focus',
+  autoHide: false,
+}
+const clearValue = () => {
+  if (!props.disabled) {
+    localValue.value = null
+  }
+}
+
+watch(isDark, console.log)
 </script>
