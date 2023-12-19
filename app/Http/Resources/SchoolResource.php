c9ba49dbddabe41f26026b7238b1b8720dae66a7
Grant Holle <hollegrant@gmail.com>
2023-12-19 14:11:54 +0400

    Add additional keys to resource

--- app/Http/Resources/SchoolResource.php
+++ app/Http/Resources/SchoolResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources;
 
+use App\Models\School;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/** @property-read School $resource */
 class SchoolResource extends JsonResource
 {
     /**
@@ -15,8 +17,18 @@ class SchoolResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'timezone' => $this->resource->timezone,
+            'allow_online_meetings' => $this->resource->allow_online_meetings,
+            'allow_translator_requests' => $this->resource->allow_translator_requests,
+            'booking_buffer_hours' => $this->resource->booking_buffer_hours,
+            'open_for_contacts_at' => $this->resource->open_for_contacts_at,
+            'close_for_contacts_at' => $this->resource->close_for_contacts_at,
+            'open_for_teachers_at' => $this->resource->open_for_teachers_at,
+            'close_for_teachers_at' => $this->resource->close_for_teachers_at,
+            'contacts_can_book' => $this->resource->contacts_can_book,
+            'teachers_can_create' => $this->resource->teachers_can_create,
         ];
     }
 }
