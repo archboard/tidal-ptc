fb8e91c2a1ce13773253e623a05f97f1f578c547
Grant Holle <hollegrant@gmail.com>
2024-01-26 10:13:58 +0400

    Update users index page

--- resources/js/pages/users/Index.vue
+++ resources/js/pages/users/Index.vue
@@ -1,8 +1,24 @@
 <template>
   <Authenticated>
-    <Table>
+    <Filters
+      v-model="filters"
+      v-model:search="search"
+      :available-filters="availableFilters"
+      @update="updateResults()"
+      :loading="updatingResults"
+    />
+
+    <div v-if="selection.length > 0" class="px-4 sm:px-5 py-3 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-600 text-sm">
+      <div class="flex items-center space-x-2">
+        <span>{{ __(':count selected', { count: selection.length }) }}</span>
+        <AppLink is="button" @click.prevent="selectedAll = false">{{ __('Remove selection') }}</AppLink>
+      </div>
+    </div>
+
+    <Table no-top-radius>
       <Thead>
         <tr>
+          <Th class="pr-0 w-4"><Checkbox @change="selectedAll = !selectedAll" :checked="selectedAll" /></Th>
           <Th>{{ __('Name') }}</Th>
           <Th>{{ __('Email') }}</Th>
           <Th>{{ __('Type') }}</Th>
@@ -11,6 +27,9 @@
       </Thead>
       <Tbody>
         <tr v-for="user in users.data" :key="user.id">
+          <Td class="pr-0 w-4">
+            <Checkbox v-model="selection" :value="user.id" @change="toggleSelection(user.id)" />
+          </Td>
           <Td>{{ user.name }}</Td>
           <Td>{{ user.email }}</Td>
           <Td>{{ user.user_type_display }}</Td>
@@ -27,6 +46,12 @@
             </ContextMenu>
           </ActionColumn>
         </tr>
+
+        <tr v-if="users.data.length === 0">
+          <Td class="text-center" colspan="5">
+            {{ __('No results found.') }}
+          </Td>
+        </tr>
       </Tbody>
     </Table>
 
@@ -39,11 +64,19 @@ import Authenticated from '@/layouts/Authenticated.vue'
 import { ActionColumn, Table, Tbody, Th, Thead } from '@/components/tables/index.js'
 import Td from '@/components/tables/Td.vue'
 import Pagination from '@/components/tables/Pagination.vue'
-import AppLink from '@/components/AppLink.vue'
 import ContextMenu from '@/components/ContextMenu.vue'
 import AppMenuItem from '@/components/AppMenuItem.vue'
+import Checkbox from '@/components/forms/Checkbox.vue'
+import useModelSelection from '@/composition/useModelSelection.js'
+import Filters from '@/components/tables/Filters.vue'
+import useFilters from '@/composition/useFilters.js'
+import AppLink from '@/components/AppLink.vue'
 
 const props = defineProps({
   users: Object,
+  availableFilters: Array,
+  currentFilters: [Array, Object],
 })
+const { selection, selectedAll, toggleSelection, selectNone } = useModelSelection('user')
+const { filters, search, updateResults, updatingResults } = useFilters()
 </script>
