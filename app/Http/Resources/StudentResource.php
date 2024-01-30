e5ff8cd014a0fe2a854fd81037bd942719f2ad92
Grant Holle <hollegrant@gmail.com>
2024-01-30 09:08:51 +0400

    Rename attribute

--- app/Http/Resources/StudentResource.php
+++ app/Http/Resources/StudentResource.php
@@ -26,7 +26,7 @@ class StudentResource extends JsonResource
             'last_name' => $this->resource->last_name,
             'email' => $this->resource->email,
             'grade_level' => $this->resource->grade_level,
-            'hidden' => $this->resource->hidden,
+            'can_book' => $this->resource->can_book,
             'sections' => SectionResource::collection($this->whenLoaded('sections')),
         ];
     }
