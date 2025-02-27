75fcd031ac507f7a407b7085f8661d500142cd67
Grant Holle <hollegrant@gmail.com>
2025-02-27 12:11:53 -0600

    Make condensed disabled pill

--- resources/js/components/BookingDisabledPill.vue
+++ resources/js/components/BookingDisabledPill.vue
@@ -1,7 +1,23 @@
 <template>
-  <Pill color="yellow">{{ __('Booking disabled') }}</Pill>
+  <span class="inline-flex">
+    <Pill v-if="expanded" color="yellow">
+      {{ __('Booking disabled') }}
+    </Pill>
+    <span v-else>
+      <LockClosedIcon class="h-4 w-4 text-yellow-500" />
+      <span class="sr-only">{{ __('Booking disabled') }}</span>
+    </span>
+  </span>
 </template>
 
 <script setup>
 import Pill from '@/components/Pill.vue'
+import { LockClosedIcon } from '@heroicons/vue/24/solid/index.js'
+
+const props = defineProps({
+  expanded: {
+    type: Boolean,
+    default: false,
+  },
+})
 </script>
